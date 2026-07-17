c288c3c100d55df484f89329620d123b8ff0e2ea
summerspringwei <120237776@qq.com>
2026-07-17 16:42:03 +0000

    Regenerate SPEC mcf_r/lbm_r shims with the simplified generic wrapper

--- SPEC_shim_root/mcf_r/SPEC_shim/kernels/mcf_r/mcf_r.c
+++ SPEC_shim_root/mcf_r/SPEC_shim/kernels/mcf_r/mcf_r.c
@@ -5,10 +5,6 @@
 #define NDEBUG 1
 #endif
 
-#include <polybench.h>
-#include <unistd.h>
-#include <fcntl.h>
-
 /**************************************************************************
 MCF.H of ZIB optimizer MCF, SPEC version
 
@@ -220,49 +216,12 @@ int kernel_mcf_r( argc, argv )
 }
 
 
-static double _checksum_file(const char* path)
-{
-  FILE* f = fopen(path, "rb");
-  if (!f) return -1.0;
-  double sum = 0.0;
-  long i = 0;
-  int c;
-  while ((c = fgetc(f)) != EOF) { sum += (double)c * (1.0 + (double)(i % 97)); i++; }
-  fclose(f);
-  return sum;
-}
-
-static void _print_checksum(double chk)
-{
-  POLYBENCH_DUMP_START;
-  POLYBENCH_DUMP_BEGIN("checksum");
-  fprintf(POLYBENCH_DUMP_TARGET, "%.6f", chk);
-  POLYBENCH_DUMP_END("checksum");
-  POLYBENCH_DUMP_FINISH;
-}
-
 int main(int argc, char** argv)
 {
   (void)argc; (void)argv;
   char* fargv[] = { "kernel_mcf_r", "/home/hanning/spec2017/benchspec/CPU/505.mcf_r/data/test/input/inp.in", "" , NULL };
   int fargc = 3;
-  const char* out_path = "/home/hanning/comet/tmp/mcf_r_stdout.tmp";
 
-  fflush(stdout);
-  int _saved_stdout = dup(1);
-  int _out_fd = open(out_path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-  if (_out_fd >= 0) { dup2(_out_fd, 1); close(_out_fd); }
-
-  polybench_start_instruments;
   kernel_mcf_r(fargc, fargv);
-  fflush(stdout);
-  polybench_stop_instruments;
-
-  dup2(_saved_stdout, 1);
-  close(_saved_stdout);
-  polybench_print_instruments;
-
-  double chk = _checksum_file(out_path);
-  polybench_prevent_dce(_print_checksum(chk));
   return 0;
 }
